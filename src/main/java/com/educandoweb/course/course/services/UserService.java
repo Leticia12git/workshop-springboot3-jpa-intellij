f8bc8492d1cf6f6f83754425f4b300c48e9ff9bc
Leticia12 <leticia.silva@arphoenix.com.br>
2023-03-17 14:43:22 -0300

    Service layer, component registration

--- src/main/java/com/educandoweb/course/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/course/services/UserService.java
@@ -1,4 +1,26 @@
 package com.educandoweb.course.course.services;
 
+import com.educandoweb.course.course.entities.User;
+import com.educandoweb.course.course.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
 public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> findAll(){
+        return userRepository.findAll();
+    }
+    public User findById(Long id){
+        Optional<User> obj = userRepository.findById(id)
+        return obj.get();
+    }
 }
