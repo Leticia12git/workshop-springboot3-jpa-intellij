75ae6f8b672a37f58a4fe3a2b8893c4929bfb546
Leticia12 <leticia.silva@arphoenix.com.br>
2023-03-17 13:22:09 -0300

    User entity and resource

--- src/main/java/com/educandoweb/course/course/entities/User.java
+++ src/main/java/com/educandoweb/course/course/entities/User.java
@@ -5,13 +5,22 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serial;
+import java.io.Serializable;
 
 @Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class User {
+public class User implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
--- src/main/java/com/educandoweb/course/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/course/resources/UserResource.java
@@ -1,15 +1,22 @@
 package com.educandoweb.course.course.resources;
 
+import com.educandoweb.course.course.entities.User;
+import com.educandoweb.course.course.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-import javax.persistence.Entity;
-
-@Entity
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
+@RestController
+@RequestMapping(value = "/users")
 public class UserResource {
+
+    @Autowired
+    private UserService userService;
+    @GetMapping
+    public ResponseEntity<User> findAll(){
+        User u = new User(1L, "Maria", "maria@gmail.com", "99999999", "1234");
+        return ResponseEntity.ok().body(u);
+    }
 }
