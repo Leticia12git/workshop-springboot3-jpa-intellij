281d8fbae66835834063182780599f2b38400df4
Leticia12 <leticia.silva@arphoenix.com.br>
2023-03-17 14:00:04 -0300

    H2 database, test profile, JPA

--- src/main/java/com/educandoweb/course/course/entities/User.java
+++ src/main/java/com/educandoweb/course/course/entities/User.java
@@ -4,10 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.io.Serial;
 import java.io.Serializable;
 
@@ -15,6 +12,7 @@ import java.io.Serializable;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Table(name = "tb_user")
 public class User implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
